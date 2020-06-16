5fc15b88c1d61e07cdb761a70d7f6a9e06637640
chiharu.kuwabara <chiharu-kuwabara@m3.com>
2020-06-16 23:46:28 +0900

    ec-webのpom用意

---
+++ services/ec-web/src/main/kotlin/com/example/distributedec/EcApplication.kt
@@ -0,0 +1,31 @@
+package com.example.distributedec
+
+import io.micrometer.core.instrument.config.MeterFilter
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.context.support.beans
+
+@SpringBootApplication
+class EcApplication
+
+val beans = beans {
+    bean("meterFilter") {
+        MeterFilter.deny() {
+            it.getTag("uri")?.startsWith("/actuator") ?: false
+        }
+    }
+}
+
+class BeansInitializer : ApplicationContextInitializer<GenericApplicationContext> {
+    override fun initialize(applicationContext: GenericApplicationContext) {
+        beans.initialize(applicationContext)
+    }
+}
+
+fun main(args: Array<String>) {
+    SpringApplicationBuilder(EcApplication::class.java)
+            .properties("context.initializer.classes=${BeansInitializer::class.java.name}")
+            .run(*args)
+}
