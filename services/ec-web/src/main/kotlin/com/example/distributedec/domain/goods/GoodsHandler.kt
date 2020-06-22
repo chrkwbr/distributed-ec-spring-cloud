016c1ed5c5ccbd361eda9a365e54a6b452259ebd
chiharu.kuwabara <chiharu-kuwabara@m3.com>
2020-06-23 07:55:12 +0900

    Repositoryがインスタンス化されていない状態

--- services/ec-web/src/main/kotlin/com/example/distributedec/domain/goods/GoodsHandler.kt
+++ services/ec-web/src/main/kotlin/com/example/distributedec/domain/goods/GoodsHandler.kt
@@ -1,14 +1,10 @@
 package com.example.distributedec.domain.goods
 
-import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
+import org.springframework.transaction.annotation.Transactional
 
-class GoodsHandler {
-    fun findById(id: Long) = Mono.just(Goods(id, "mono goods"))
+@Transactional
+class GoodsHandler(private val goodsRepository: GoodsRepository) {
+    fun findById(id: Long) = goodsRepository.findById(id)
 
-    fun findAll() = Flux.just(
-            Goods(1, "taro"),
-            Goods(2, "jiro"),
-            Goods(3, "saburo")
-    )
+    fun findAll() = goodsRepository.findAll()
 }
